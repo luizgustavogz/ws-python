7da87d9514f46345da133b0c5934076ed816f46f
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-05-31 16:32:49 -0300

    Update Aula1.py e Aula12.py

--- Curso_Python/Aula1.py
+++ Curso_Python/Aula1.py
@@ -1,4 +1,2 @@
-# Aula 1 - Introdução ao Python 30/05/2023
-
 print(123)
 print('Luiz Gustavo')
--- Curso_Python/Aula12.py
+++ Curso_Python/Aula12.py
@@ -1,6 +1,6 @@
 nome = 'Luiz Gustavo'
 altura = 1.83
-peso = 130
+peso = 128
 imc = peso / altura ** 2
 
 print(nome, 'tem', altura, 'm de altura, pesa', peso, 'kg e seu IMC é', imc)
