4a1de3c09e34faedf7029bdd1da9beadcc1d30c5
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-05-31 16:50:31 -0300

    Create Aula15.py

---
+++ Curso_Python/Aula15.py
@@ -0,0 +1,10 @@
+# nome = input('Qual o seu nome? ')
+# print(f'O seu nome é {nome}')
+
+num_1 = input('Digite um número: ')
+num_2 = input('Digite outro número: ')
+
+int_num_1 = int(num_1)
+int_num_2 = int(num_2)
+
+print(f'A soma dos números é = {int_num_1 + int_num_2}')
