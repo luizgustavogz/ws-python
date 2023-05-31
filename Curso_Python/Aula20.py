28181fa32fce11226cc84b4f0276327f556646b2
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-05-31 17:30:54 -0300

    Create Aula20.py

---
+++ Curso_Python/Aula20.py
@@ -0,0 +1,7 @@
+primeiro_valor = input('Digite o primeiro valor: ')
+segundo_valor = input('Digite o segundo valor: ')
+
+if primeiro_valor >= segundo_valor:
+    print(f'{primeiro_valor=} é maior ou igual ao que {segundo_valor=}')
+else:
+    print(f'{segundo_valor=} é maior do que {primeiro_valor=}')
