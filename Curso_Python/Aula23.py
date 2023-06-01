03c3c407b499e19e155a9fe5581ad9de4c880667
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-06-01 14:19:24 -0300

    Create Aula23.py

---
+++ Curso_Python/Aula23.py
@@ -0,0 +1,16 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True -> False
+# not False -> True
+senha = input('Senha: ')
+
+# if senha != '123456':
+    # print('Senha incorreta.')
+
+if not senha:
+    print('Senha incorreta.')
+
+# print(not True)
+# print(not False)
+# print(not 0)
+# print(not 1)
