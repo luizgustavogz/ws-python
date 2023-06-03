6d6f39ae5fa0de0314d0efb19643b584f4763c7c
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-06-03 15:52:07 -0300

    Create Aula39.py

---
+++ Curso_Python/Aula39.py
@@ -0,0 +1,13 @@
+"""
+Iterando strings com while
+"""
+nome = 'Luiz Gustavo'
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f'*{letra}'
+    indice += 1
+
+novo_nome += '*'
+print(novo_nome)
