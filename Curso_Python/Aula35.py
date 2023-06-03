15e79f7c4ec12d453f3e7c469f873d01383b7c60
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-06-03 15:25:37 -0300

    Create Aula35.py

---
+++ Curso_Python/Aula35.py
@@ -0,0 +1,12 @@
+"""
+Repetições -> while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+contador = 0
+
+while contador <= 10:
+    print(contador)
+    contador += 1
+
+print('Fim do programa')
