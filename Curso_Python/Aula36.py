d2c77bdc3bf91ca795229f7fc5aa264345e03a3a
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-06-03 15:31:40 -0300

    Create Aula36.py

---
+++ Curso_Python/Aula36.py
@@ -0,0 +1,20 @@
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+contador = 10
+contador2 = 10
+contador3 = 10 
+contador4 = 10
+
+contador *= '2'
+print(contador)
+
+contador3 **=2
+print(contador3)
+
+contador4 //=2
+print(contador4)
+
+contador2 %=2
+print(contador2)
