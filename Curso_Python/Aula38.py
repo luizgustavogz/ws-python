9eeb4156cb2f56276ae4877c13209fde8238f0bd
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-06-03 15:45:17 -0300

    Create Aula38.py

---
+++ Curso_Python/Aula38.py
@@ -0,0 +1,18 @@
+"""
+Repetições -> while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+while linha <= qtd_linhas:
+    coluna = 1
+    while coluna <= qtd_colunas:
+        print(f'{linha=} {coluna=} \t', end='')
+        coluna += 1
+    linha += 1
+    print('\n')
+
+print('Fim do programa')
