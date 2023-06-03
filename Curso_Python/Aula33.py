43694d2f6acd8ecf7cb414abc1b4b66eef5e49a1
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-06-03 15:13:54 -0300

    Create Aula33.py

---
+++ Curso_Python/Aula33.py
@@ -0,0 +1,10 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+string = 'luiz Gustavo'
+outra_string = f'{string[:3]}ABC{string[4:]}'
+print(string)
+print(outra_string)
+# print(string.capitalize())
+# print(string.zfill(100))
