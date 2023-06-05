bf7ce6db57fa61be1278e334a3eefa96f6a9d80b
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-06-05 14:09:47 -0300

    Create Aula48.py

---
+++ Curso_Python/Aula48.py
@@ -0,0 +1,15 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis - append, insert, pop, del, clear, extend, +
+"""
+string = 'ABCDE'
+# print(bool([])) # falsy
+# print(lista, type(lista))
+
+lista = [123, True, 'Luiz Gustavo', 1.2, []]
+lista[-3] = 'Larissa'
+print(lista)
+print(lista[2], type(lista[2]))
