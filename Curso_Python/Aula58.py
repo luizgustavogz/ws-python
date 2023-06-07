568ce0a77030756482bff476fb5c6bd7e5bf1336
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-06-07 13:19:10 -0300

    Create Aula58.py

---
+++ Curso_Python/Aula58.py
@@ -0,0 +1,30 @@
+"""
+Interpretador do Python
+python mod.py (executa o mod)
+python -u (unbuffered)
+pythom -m mod (lib mod como script)
+python -c 'cmd' (executa o comando)
+python -i mod.py (interativo com mod)
+
+The Zen of Python, por Tim Peters
+
+Bonito é melhor que feio.
+Explícito é melhor que implícito.
+Simples é melhor que complexo.
+Complexo é melhor que complicado.
+Linear é melhor que aninhado.
+Esparso é melhor que denso.
+Legibilidade conta.
+Casos especiais não são especiais o bastante para quebrar as regras.
+Ainda que praticidade vença a pureza.
+Erros nunca devem passar silenciosamente.
+A menos que sejam explicitamente silenciados.
+Diante da ambiguidade, recuse a tentação de adivinhar.
+Deveria haver um — e preferencialmente só um — modo óbvio para fazer algo.
+Embora esse modo possa não ser óbvio a princípio a menos que você seja holandês.
+Agora é melhor que nunca.
+Embora nunca freqüentemente seja melhor que já.
+Se a implementação é difícil de explicar, é uma má idéia.
+Se a implementação é fácil de explicar, pode ser uma boa idéia.
+Namespaces são uma grande idéia — vamos ter mais dessas!
+"""
