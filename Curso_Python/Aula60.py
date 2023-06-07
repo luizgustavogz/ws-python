6c54fce6472b6e302d894a48157551d45c666144
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-06-07 13:45:02 -0300

    Create Aula60.py

---
+++ Curso_Python/Aula60.py
@@ -0,0 +1,15 @@
+"""
+Operação ternária (condicional de uma linha)
+<valor> if <condição> else <outro valor>
+"""
+
+# condicao = True
+# variavel = 'Valor' if condicao else 'Outro valor'
+# print(variavel)
+
+# digito = 9
+# novo_digito = digito if digito <= 9 else 0
+# novo_digito = 0 if digito > 9 else digito
+# print(novo_digito)
+
+print('Valor' if False else 'Outro valor' if True else 'Fim')
