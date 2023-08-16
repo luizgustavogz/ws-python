a6900e9ed6a902fa28c771dcd519271e67416340
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-08-16 11:53:50 -0300

    Create Aula99.py and Aula99_package

---
+++ Curso_Python/Aula99.py
@@ -0,0 +1,14 @@
+# from sys import path
+# print(*path, sep='\n')
+
+import Aula99_package.modulo
+from Aula99_package import modulo
+from Aula99_package.modulo import *
+from Aula99_package.modulo import soma_do_modulo as sdm
+
+print(soma_do_modulo(1, 2))
+print(Aula99_package.modulo.soma_do_modulo(2, 3))
+print(modulo.soma_do_modulo(1, 2))
+print(sdm(2, 3))
+print(variavel)
+print(nova_variavel)
---
+++ Curso_Python/Aula99_package/modulo.py
@@ -0,0 +1,10 @@
+__all__ = [
+    'variavel', 'soma_do_modulo', 'nova_variavel',
+]
+
+variavel = 'variável do módulo'
+
+def soma_do_modulo(x, y):
+    return x + y
+
+nova_variavel = 'nova variável do módulo'
