3c0f083ee90003fae21e7b2b3fb174ffbd31ebaa
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-09-12 11:40:28 -0300

    Create Aula122.py

---
+++ Aula122.py
@@ -0,0 +1,23 @@
+# Entendendo self em classes Python
+# Classe - Molde (geralmente sem dados)
+# Instância da class (objeto) - Tem os dados
+# Uma classe pode gerar várias instâncias.
+# Na classe o self é a própria instância.
+class Carro:
+    def __init__(self, modelo):
+        self.nome = modelo
+
+    def acelerar(self):
+        print(f'{self.nome} está acelerando...')
+
+
+fusca = Carro('Fusca')
+fusca.acelerar()
+Carro.acelerar(fusca)
+# print(fusca.nome)
+# fusca.acelerar()
+
+celta = Carro(modelo='Celta')
+celta.acelerar()
+Carro.acelerar(celta)
+# print(celta.nome)
