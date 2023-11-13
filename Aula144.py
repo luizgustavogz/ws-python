a3f620f5f12a788f4bd555705b401fd46f67ec71
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-11-13 15:05:37 -0300

    Create Aula144.py

---
+++ Aula144.py
@@ -0,0 +1,17 @@
+# Polimorfismo em Python Orientado a Objetos
+# Polimorfismo é o princípio que permite que classes derivadasde uma superclasse
+# tenham métodos iguais (com mesma assinatura) mas comportamentos diferentes.
+# Assinatura do método = Mesmo nome e quantidade de parâmetros (retorno não faz
+# parte da assinatura).
+# Opinião + princípios que contam:
+# Assinatura do método: nome, parâmetros e retorno iguais.
+# SOLID
+# S - Single Responsability Principle (SRP) - Princípio da responsabilidade única
+# O - Open-Closed Principle (OCP) - Princípio aberto-fechado
+# L - Liskov Substitution Principle (LSP) - Princípio da substituição de Liskov
+# I - Interface Segregation Principle (ISP) - Princípio da segregação de interfaces
+# D - Dependency Inversion Principle (DIP) - Princípio da inversão de dependência
+# SO"L"ID
+# Princípio da substituição de Liskov
+# Objetos de uma superclasse devem ser substituíveis por objetos de uma
+# subclasse sem quebrar a aplicação.
