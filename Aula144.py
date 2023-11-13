835a25e3312f719091ba3bf245c3f73239df961a
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-11-13 15:20:21 -0300

    Update Aula144.py

--- Aula144.py
+++ Aula144.py
@@ -15,3 +15,42 @@
 # Princípio da substituição de Liskov
 # Objetos de uma superclasse devem ser substituíveis por objetos de uma
 # subclasse sem quebrar a aplicação.
+# Sobrecarga de métodos (overload) - Pyhton não suporta sobrecarga de métodos
+# Sobreposição de métodos (override) - Python suporta sobreposição de métodos
+from abc import ABC, abstractmethod
+
+
+class Notificacao(ABC):
+    def __init__(self, mensagem) -> None:
+        self.mensagem = mensagem
+
+    @abstractmethod
+    def enviar(self) -> bool: ...
+
+
+class NotificacaoEmail(Notificacao):
+    def enviar(self) -> bool:
+        print(f'Enviando e-mail: {self.mensagem}')
+        return True
+
+
+class NotificacaoSMS(Notificacao):
+    def enviar(self) -> bool:
+        print(f'Enviando SMS: {self.mensagem}')
+        return False
+
+
+def notificar(notificacao: Notificacao):
+    notificacao_enviada = notificacao.enviar()
+
+    if notificacao_enviada:
+        print('Notificação enviada com sucesso\n')
+    else:
+        print('Falha ao enviar notificação\n')
+
+
+notificacao_email = NotificacaoEmail('Testando e-mail')
+notificar(notificacao_email)
+
+notificacao_sms = NotificacaoSMS('Testando SMS')
+notificar(notificacao_sms)
