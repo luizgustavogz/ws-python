5fb8d827cc3933f6a6f54c0fa414f035e1fcbfe1
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-11-14 12:22:19 -0300

    Update Aula147.py

--- Aula147.py
+++ Aula147.py
@@ -15,3 +15,24 @@
 # __neg__(self) - -self
 # __str__(self) - str
 # __repr__(self) - str
+class Ponto:
+    def __init__(self, x, y, z='String'):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):  # Se não for definido, o fallback é o __repr__
+        return f'({self.x}, {self.y})'
+
+    def __repr__(self):  # Voltado para devs (saber como o objeto foi montado)
+        # class_name = self.__class__.__name__
+        class_name = type(self).__name__
+        return f'{class_name} (x={self.x!r}, y={self.y!r}, z={self.z!r})'
+
+
+p1 = Ponto(1, 2)
+p2 = Ponto(978, 876)
+print(p1)
+print(repr(p2))
+print(f'{p2!s}')    # !s = str
+print(f'{p2!r}')    # !r = repr
