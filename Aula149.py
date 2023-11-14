76199155bae57ce85becdb36062bfdc174050f63
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-11-14 13:22:29 -0300

    Update Aula149.py

--- Aula149.py
+++ Aula149.py
@@ -19,7 +19,7 @@ class MyOpen:
     def __init__(self, caminho_arquivo, modo):
         self.caminho_arquivo = caminho_arquivo
         self.modo = modo
-        self._arquivo = None
+        # self._arquivo = None
 
     def __enter__(self):
         print('Abrindo arquivo')
@@ -28,8 +28,7 @@ class MyOpen:
 
     def __exit__(self, class_exception, exception_, traceback_):
         print('Fechando arquivo')
-        if self._arquivo is not None:
-            self._arquivo.close()
+        self._arquivo.close()
 
         # raise class_exception(*exception_.args).with_traceback(traceback_)
 
