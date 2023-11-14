ea7f591bd22581a368761b42e0c183e6ee1acdf1
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-11-14 12:33:54 -0300

    Update Aula147.py

--- Aula147.py
+++ Aula147.py
@@ -16,23 +16,36 @@
 # __str__(self) - str
 # __repr__(self) - str
 class Ponto:
-    def __init__(self, x, y, z='String'):
+    def __init__(self, x, y):  # z='String'
         self.x = x
         self.y = y
-        self.z = z
+        # self.z = z
 
-    def __str__(self):  # Se não for definido, o fallback é o __repr__
-        return f'({self.x}, {self.y})'
+    # def __str__(self):  # Se não for definido, o fallback é o __repr__
+    #     return f'({self.x}, {self.y})'
 
     def __repr__(self):  # Voltado para devs (saber como o objeto foi montado)
         # class_name = self.__class__.__name__
         class_name = type(self).__name__
-        return f'{class_name} (x={self.x!r}, y={self.y!r}, z={self.z!r})'
+        return f'{class_name} (x={self.x!r}, y={self.y!r})'  # z={self.z!r}
+
+    def __add__(self, other):
+        return Ponto(self.x + other.x, self.y + other.y)
+
+    def __gt__(self, other):
+        return self.x + self.y > other.x + other.y
+
+# p1 = Ponto(1, 2)
+# p2 = Ponto(978, 876)
+# print(p1)
+# print(f'{p2!s}')    # !s = str
+# print(f'{p2!r}')    # !r = repr
 
 
-p1 = Ponto(1, 2)
-p2 = Ponto(978, 876)
-print(p1)
-print(repr(p2))
-print(f'{p2!s}')    # !s = str
-print(f'{p2!r}')    # !r = repr
+if __name__ == '__main__':
+    p1 = Ponto(4, 2)
+    p2 = Ponto(6, 4)
+    p3 = p1 + p2
+    print(p3)
+    print('P1 é maior que P2?', p1 > p2)
+    print('P2 é maior que P1?', p2 > p1)
