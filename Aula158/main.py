77c7e96cb3898e1a3dd6428693512a4519977a71
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-11-22 13:50:13 -0300

    Update Aula158.py
    Create autenticar in banco.py

--- Aula158/main.py
+++ Aula158/main.py
@@ -33,5 +33,5 @@ Banco será responsável autenticar o cliente e as contas da seguinte maneira:
     * Checar se o cliente é daquele banco
     * Checar se a conta é daquele banco
 Só será possível sacar se passar na autenticação do banco (descrita acima)
-Banco autentica por um método.
+Banco autentica por um método (autenticar).
 """
