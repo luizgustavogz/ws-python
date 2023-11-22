08c36bc4a77bb73fd851bcb9f2577a45e00b4829
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-11-22 13:08:14 -0300

    Update Aula158.py
    Criando classe ContaCorrente + linters + settings.json

--- Aula158/contas.py
+++ Aula158/contas.py
@@ -2,39 +2,73 @@ import abc
 
 
 class Conta(abc.ABC):
-    def __init__(self, agencia, conta, saldo):
+    def __init__(self, agencia: int, conta: int, saldo: float = 0) -> None:
         self.agencia = agencia
         self.conta = conta
         self.saldo = saldo
 
     @abc.abstractmethod
-    def sacar(self, valor): ...
+    def sacar(self, valor: float) -> float: ...
 
-    def depositar(self, valor):
+    def depositar(self, valor: float) -> float:
         self.saldo += valor
-        self.detalhes(f'(Depósito {valor})')
+        self.detalhes(f'(Valor depositado R${valor:.2f})')
+        return self.saldo
 
-    def detalhes(self, msg=''):
-        print(f'O seu saldo é {self.saldo:.2f} {msg}')
+    def detalhes(self, msg: str = '') -> None:
+        print(f'Saldo atual de R${self.saldo:.2f} {msg}')
         print('--')
 
 
 class ContaPoupanca(Conta):
-    def sacar(self, valor):
+    def sacar(self, valor: float) -> float:
         valor_pos_saque = self.saldo - valor
 
         if valor_pos_saque >= 0:
             self.saldo -= valor
-            self.detalhes(f'(Saque {valor})')
+            self.detalhes(f'(Sacado R${valor:.2f})')
             return self.saldo
 
         print('Não foi possível sacar o valor desejado')
-        self.detalhes(f'(Saque negado {valor})')
+        self.detalhes(f'(Saque negado de R${valor:.2f})')
+        return self.saldo
+
+
+class ContaCorrente(Conta):
+    def __init__(
+        self, agencia: int, conta: int,
+        saldo: float = 0, limite: float = 0
+    ):
+        super().__init__(agencia, conta, saldo=0)
+        self.limite = limite
+
+    def sacar(self, valor: float) -> float:
+        valor_pos_saque = self.saldo - valor
+        limite_max = -self.limite
+
+        if valor_pos_saque >= limite_max:
+            self.saldo -= valor
+            self.detalhes(f'(Sacado R${valor:.2f})')
+            return self.saldo
+
+        print('Não foi possível sacar o valor desejado')
+        print(f'Seu limite é de R${-self.limite:.2f}')
+        self.detalhes(f'(Saque negado de R${valor:.2f})')
+        return self.saldo
 
 
 if __name__ == '__main__':
-    cp1 = ContaPoupanca(111, 222, 0)
+    cp1 = ContaPoupanca(111, 222)
     cp1.sacar(1)
     cp1.depositar(1)
     cp1.sacar(1)
     cp1.sacar(1)
+    print('##')
+    cc1 = ContaCorrente(111, 222, 0, 100)
+    cc1.sacar(1)
+    cc1.depositar(1)
+    cc1.sacar(1)
+    cc1.sacar(1)
+    cc1.sacar(98)
+    cc1.sacar(1)
+    print('##')
--- Aula158/main.py
+++ Aula158/main.py
@@ -3,15 +3,18 @@ Exercício com Abstração, Herança, Encapsulamento e Polimorfismo
 Criar um sistema bancário (extremamente simples) que tem clientes, contas e
 um banco. A ideia é que o cliente tenha uma conta (poupança ou corrente) e que
 possa sacar/depositar nessa conta. Contas corrente tem um limite extra.
+
 Conta (ABC)
     ContaCorrente
     ContaPoupanca
-Pessoa (ABC)
+
+Pessoa
     Cliente
         Clente -> Conta
 Banco
     Banco -> Cliente
     Banco -> Conta
+
 Dicas:
 Criar classe Cliente que herda da classe Pessoa (Herança)
     Pessoa tem nome e idade (com getters)
