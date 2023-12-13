b5398ba0ff06e162e29b1874051db8922703176f
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-12-13 11:58:13 -0300

    Update Aula159.py

--- Aula159.py
+++ Aula159.py
@@ -5,37 +5,53 @@
 # Em resumo: dataclasses são syntax sugar para criar classes normais.
 # Foi descrito na PEP 557 e adicionado na versão 3.7 do Python.
 # doc: https://docs.python.org/3/library/dataclasses.html
-from dataclasses import dataclass, asdict, astuple
+from dataclasses import dataclass, asdict, astuple, field, fields
+
+
+# @dataclass
+# class Pessoa:
+#     nome: str
+#     sobrenome: str
+
+#     # def __init__(self, nome, sobrenome) -> None:
+#     #     self.nome = nome
+#     #     self.sobrenome = sobrenome
+#     #     self.nome_completo = f'{self.nome} {self.sobrenome}'
+
+#     # def __post_init__(self) -> None:
+#     #     self.nome_completo = f'{self.nome} {self.sobrenome}'
+
+#     # @property
+#     # def nome_completo(self) -> str:
+#     #     return f'{self.nome} {self.sobrenome}'
+
+#     # @nome_completo.setter
+#     # def nome_completo(self, nome_completo: str) -> None:
+#     #     self.nome, self.sobrenome = nome_completo.split(' ', maxsplit=1)
+
+
+# if __name__ == '__main__':
+#     # lista = [Pessoa('A', 'Z'), Pessoa('B', 'Y'), Pessoa('C', 'X')]
+#     # ordenadas = sorted(lista, reverse=True, key=lambda p: p.sobrenome)
+#     # print(ordenadas)
+#     p1 = Pessoa('Luiz', 'Cunha')
+#     print(asdict(p1).keys())
+#     print(asdict(p1).values())
+#     print(asdict(p1).items())
+#     print(astuple(p1)[0])
 
 
 @dataclass
 class Pessoa:
-    nome: str
-    sobrenome: str
-
-    # def __init__(self, nome, sobrenome) -> None:
-    #     self.nome = nome
-    #     self.sobrenome = sobrenome
-    #     self.nome_completo = f'{self.nome} {self.sobrenome}'
-
-    # def __post_init__(self) -> None:
-    #     self.nome_completo = f'{self.nome} {self.sobrenome}'
-
-    # @property
-    # def nome_completo(self) -> str:
-    #     return f'{self.nome} {self.sobrenome}'
-
-    # @nome_completo.setter
-    # def nome_completo(self, nome_completo: str) -> None:
-    #     self.nome, self.sobrenome = nome_completo.split(' ', maxsplit=1)
+    nome: str = field(
+        default='MISSING', repr=False
+    )
+    sobrenome: str = 'Not sent'
+    idade: int = 100
+    enderecos: list[str] = field(default_factory=list)
 
 
 if __name__ == '__main__':
-    # lista = [Pessoa('A', 'Z'), Pessoa('B', 'Y'), Pessoa('C', 'X')]
-    # ordenadas = sorted(lista, reverse=True, key=lambda p: p.sobrenome)
-    # print(ordenadas)
-    p1 = Pessoa('Luiz', 'Cunha')
-    print(asdict(p1).keys())
-    print(asdict(p1).values())
-    print(asdict(p1).items())
-    print(astuple(p1)[0])
+    p1 = Pessoa()
+    # print(fields(p1))
+    print(p1)
