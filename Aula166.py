8b338c59c0474d607ecce3d3ff0ba04fa41c5c17
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-12-21 12:32:09 -0300

    Update Aula166.py

--- Aula166.py
+++ Aula166.py
@@ -16,6 +16,13 @@ import calendar
 # print(calendar.monthrange(2023, 12))
 # print(list(enumerate(calendar.day_name)))
 
-numero_primeiro_dia, ultimo_dia = calendar.monthrange(2023, 12)
-print(calendar.day_name[numero_primeiro_dia])
-print(calendar.day_name[calendar.weekday(2023, 12, ultimo_dia)])
+# numero_primeiro_dia, ultimo_dia = calendar.monthrange(2023, 12)
+# print(calendar.day_name[numero_primeiro_dia])
+# print(calendar.day_name[calendar.weekday(2023, 12, ultimo_dia)])
+
+for week in calendar.monthcalendar(2023, 12):
+    for day in week:
+        if day == 0:
+            continue
+        print(day)
+    # print(list(enumerate(week)))
