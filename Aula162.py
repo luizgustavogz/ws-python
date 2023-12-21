072699b699f327c4f9b148701af8a246ad2a0f51
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-12-21 11:45:42 -0300

    Update Aula162.py

--- Aula162.py
+++ Aula162.py
@@ -11,13 +11,18 @@
 # Instalando o pytz
 # pip install pytz types-pytz
 from datetime import datetime
+from pytz import timezone, utc
 
-data_str_data = '2022/02/22 07:49:23'
-data_str_fmt = '%Y/%m/%d %H:%M:%S'
+# data_str_data = '2022/02/22 07:49:23'
+# data_str_fmt = '%Y/%m/%d %H:%M:%S'
 
-data_str_data = '22/02/2022'
-data_str_fmt = '%d/%m/%Y'
+# data_str_data = '22/02/2022'
+# data_str_fmt = '%d/%m/%Y'
 
 # data = datetime(2023, 2, 22, 7, 49, 23)
-data = datetime.strptime(data_str_data, data_str_fmt)
+# data = datetime.strptime(data_str_data, data_str_fmt)
+# print(data)
+
+data = datetime.now(timezone('Asia/Tokyo'))
+data = datetime(2023, 2, 22, 7, 49, 23, tzinfo=timezone('Asia/Tokyo'))
 print(data)
