c72f1607dd43a9f89e6b350b5fbf5167003396be
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2024-01-15 15:22:34 -0300

    Create Aula168.py

---
+++ Aula168.py
@@ -0,0 +1,20 @@
+# O módulo "os" para interação com o sistema
+# O módulo "os" fornece funções para interagir com o sistema operacional.
+# Por exemplo, o módulo os.path contém funções para trabalhar com caminhos de
+# arquivos e a função os.listdir() pode ser usada para listar os arquivos em um
+# diretório. O método os.system() permite executar comandos do sistema
+# operacional a partir do seu código Python.
+# Windows 11 (PowerShell), Linux (Bash), MacOS (Terminal) = clear
+# Windows antigo (CMD) = cls
+# https://docs.python.org/3/library/os.html
+import os
+
+os.system('cls')
+os.system('echo "Hello world"')
+
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
