52b29a3d836fd07a89eec102539777f6790b8d6a
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2024-01-22 11:56:16 -0300

    Update Aula177.py

--- Aula177.py
+++ Aula177.py
@@ -10,14 +10,19 @@ FILE_ABSOLUT_PATH = os.path.abspath(
     )
 )
 
-string_json = '''
-{
-  "title": "O Senhor dos Anéis: A Sociedade do Anel",
-  "original_title": "The Lord of the Rings: The Fellowship of the Ring",
-  "is_movie": true,
-  "imdb_rating": 8.8,
-  "year": 2001,
-  "characters": ["Frodo", "Sam", "Gandalf", "Legolas", "Boromir"],
-  "budget": null
+movie = {
+    'title': 'O Senhor dos Anéis: A Sociedade do Anel',
+    'original_title': 'The Lord of the Rings: The Fellowship of the Ring',
+    'is_movie': True,
+    'imdb_rating': 8.8,
+    'year': 2001,
+    'characters': ['Frodo', 'Sam', 'Gandalf', 'Legolas', 'Boromir'],
+    'budget': None
 }
-'''
+
+with open(FILE_ABSOLUT_PATH, 'w') as file:
+    json.dump(movie, file, ensure_ascii=False, indent=2)
+
+with open(FILE_ABSOLUT_PATH, 'r') as file:
+    file_content = json.load(file)
+    print(file_content)
