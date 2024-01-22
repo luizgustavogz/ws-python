8e20082e06ee19ddc4e0832e1058c6f925e5f3eb
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2024-01-22 12:26:21 -0300

    Update Aula180.py

--- Aula180.py
+++ Aula180.py
@@ -12,30 +12,30 @@ clients_list = [
     {'Nome': 'Maria Sol', 'Endereço': 'Av B, 3A'},
 ]
 
-# with open(CSV_PATH, 'w', encoding='utf-8') as file:
-#     columns_name = clients_list[0].keys()
-#     writer = csv.DictWriter(
-#         file,
-#         fieldnames=columns_name
-#     )
-#     writer.writeheader()
-
-#     for client in clients_list:
-#         print(client)
-#         writer.writerow(client)
-
-clients_list = [
-    ['Luiz Gustavo', 'Av 1, 22'],
-    ['João Silva', 'R. 2, "1"'],
-    ['Maria Sol', 'Av B, 3A'],
-]
-
-with open(CSV_PATH, 'w', encoding='utf-8') as file:
-    # columns_name = lista_clients[0].keys()
-    columns_name = ['Nome', 'Endereço']
-    writer = csv.writer(file)
-
-    writer.writerow(columns_name)
+with open(CSV_PATH, 'w', encoding='utf-8', newline='') as file:
+    columns_name = clients_list[0].keys()
+    writer = csv.DictWriter(
+        file,
+        fieldnames=columns_name
+    )
+    writer.writeheader()
 
     for client in clients_list:
+        print(client)
         writer.writerow(client)
+
+# clients_list = [
+#     ['Luiz Gustavo', 'Av 1, 22'],
+#     ['João Silva', 'R. 2, "1"'],
+#     ['Maria Sol', 'Av B, 3A'],
+# ]
+
+# with open(CSV_PATH, 'w', encoding='utf-8', newline='') as file:
+#     # columns_name = lista_clients[0].keys()
+#     columns_name = ['Nome', 'Endereço']
+#     writer = csv.writer(file)
+
+#     writer.writerow(columns_name)
+
+#     for client in clients_list:
+#         writer.writerow(client)
