b24b51b979520afdb6375f583074c9378b0c5fb2
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2024-02-02 15:08:55 -0300

    Update Aula192.py

--- Aula192.py
+++ Aula192.py
@@ -7,6 +7,7 @@
 # - Doc: https://www.crummy.com/software/BeautifulSoup/bs4/doc.ptbr/
 # + Instalação
 # - pip install requests types-requests bs4
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -16,5 +17,14 @@ raw_html = response.text  # HTML cru
 parsed_html = BeautifulSoup(raw_html, 'html.parser')  # HTML formatado
 
 # print(raw_html)
-if parsed_html.title is not None:
-    print(parsed_html.title.text)
+# if parsed_html.title is not None:
+#     print(parsed_html.title.text)
+
+top_jobs_heading = parsed_html.select_one('#intro > div > div> article > h2')
+
+if top_jobs_heading is not None:
+    article = top_jobs_heading.parent
+
+    if article is not None:
+        for p in article.select('p'):
+            print(re.sub(r'\s{1,}', ' ', p.text).strip())
