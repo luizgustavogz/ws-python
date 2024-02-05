2041e118eafd619b03c7d0620fec2f4017aab721
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2024-02-05 17:33:05 -0300

    Update main.py

--- Aula193/main.py
+++ Aula193/main.py
@@ -1,19 +1,46 @@
 from pathlib import Path
-import time
+from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+# Chrome Options: https://peter.sh/experiments/chromium-command-line-switches/
+
+
+# Caminho para a raíz do projeto
 ROOT_FOLDER = Path(__file__).parent
-CHROMEDRIVER_EXE = ROOT_FOLDER / 'drivers' / 'chromedriver.exe'
+# Caminho para a pasta onde o chromedriver está
+CHROME_DRIVER_PATH = ROOT_FOLDER / 'drivers' / 'chromedriver.exe'
 
-chrome_options = webdriver.ChromeOptions()
-chrome_services = Service(executable_path=str(CHROMEDRIVER_EXE))
-chrome_browser = webdriver.Chrome(
-    service=chrome_services,
-    options=chrome_options,
-)
 
-# chrome_options.add_argument('--headless')
+def make_chrome_browser(*options: str) -> webdriver.Chrome:
+    chrome_options = webdriver.ChromeOptions()
 
-chrome_browser.get('https://www.google.com.br/')
-time.sleep(30)
+    # chrome_options.add_argument('--headless')
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
+    chrome_service = Service(
+        executable_path=str(CHROME_DRIVER_PATH)
+    )
+
+    browser = webdriver.Chrome(
+        service=chrome_service,
+        options=chrome_options,
+    )
+
+    return browser
+
+
+if __name__ == '__main__':
+    # Exemplo
+    # options = '--headless', '--disable-gpu'
+    options = ()
+    browser = make_chrome_browser(*options)
+
+    # Abre o Google
+    browser.get('https://www.google.com')
+
+    # Espera 5 segundos
+    sleep(5)
