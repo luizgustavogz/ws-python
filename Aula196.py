efc92ad85817aadd0be7f407828353e119761192
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2024-02-06 18:14:29 -0300

    Update Aula196.py

--- Aula196.py
+++ Aula196.py
@@ -1,6 +1,8 @@
+"""
 # (Parte 1) Threads - Executando processamentos em paralelo
 from threading import Thread
 from time import sleep
+
 
 
 class MeuThread(Thread):
@@ -27,3 +29,28 @@ t3.start()
 for i in range(20):
     print(i)
     sleep(1)
+"""
+
+
+# (Parte 2) Threads - Executando processamentos em paralelo
+from threading import Thread
+from time import sleep
+
+
+def vai_demorar(texto: str, tempo: int):
+    sleep(tempo)
+    print(texto)
+
+
+t1 = Thread(target=vai_demorar, args=('Olá mundo 1!', 5))
+t1.start()
+
+t2 = Thread(target=vai_demorar, args=('Olá mundo 2!', 1))
+t2.start()
+
+t3 = Thread(target=vai_demorar, args=('Olá mundo 3!', 2))
+t3.start()
+
+for i in range(20):
+    print(i)
+    sleep(.5)
