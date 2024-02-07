0f01e4a42db67527ff5c1001abd88bff3197f2b1
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2024-02-07 15:32:18 -0300

    Create Aula197 folder
    Create main.py and downloaded R20230210.pdf

---
+++ Aula197/main.py
@@ -0,0 +1,8 @@
+# PyPDF2 - Para manipular arquivos PDF
+# PyPDF2 é uma biblioteca de manipulação de arquivos PDF feita em Python puro,
+# gratuita e de código aberto. Ela é capaz de ler, manipular, escrever e unir
+# dados de arquivos PDF, assim como adicionar anotações, transformar páginas,
+# extrair texto e imagens, manipular metadados e muito mais.
+# A documentação contém todas as informações necessárias para usar o PyPDF2.
+# https://pypdf2.readthedocs.io/en/latest/
+# pip install pypdf2
