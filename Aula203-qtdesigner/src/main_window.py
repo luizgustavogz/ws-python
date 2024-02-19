0a008c8ca3e13d38ea0875f7a7050d430330b687
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2024-02-19 11:33:01 -0300

    Update main_window.py
    eventFilter e installEventFilter

--- Aula203-qtdesigner/src/main_window.py
+++ Aula203-qtdesigner/src/main_window.py
@@ -1,6 +1,10 @@
+import sys
+from typing import cast
+
+from PySide6.QtCore import QObject, QEvent
+from PySide6.QtGui import QKeyEvent
 from PySide6.QtWidgets import QMainWindow, QWidget, QApplication
 from window import Ui_MainWindow
-import sys
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -10,9 +14,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.buttonSend.clicked.connect(self.updateLabelResult)
 
-    def updateLabelResult(self):
+        self.lineName.installEventFilter(self)
+
+    def updateLabelResult(self) -> None:
         text = self.lineName.text()
         self.labelResult.setText(text)
+
+    def eventFilter(self, watched: QObject, event: QEvent) -> bool:
+        if event.type() == QEvent.Type.KeyPress:
+            # Tenho certeza que o tipo é KeyPress
+            event = cast(QKeyEvent, event)
+            text = self.lineName.text()
+            self.labelResult.setText(text + event.text())
+
+        return super().eventFilter(watched, event)
 
 
 if __name__ == '__main__':
