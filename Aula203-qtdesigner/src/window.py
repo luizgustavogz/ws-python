9a2dfeef222bbdb9053638acea2ef35212fd66f5
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2024-02-19 10:15:37 -0300

    Create Aula203-qtdesigner
    Using designer.exe from PySide6

---
+++ Aula203-qtdesigner/src/window.py
@@ -0,0 +1,98 @@
+# pyside6-uic Aula203-qtdesigner/ui/window.ui -o Aula203-qtdesigner/src/window.py
+
+# -*- coding: utf-8 -*-
+
+################################################################################
+# Form generated from reading UI file 'window.ui'
+##
+# Created by: Qt User Interface Compiler version 6.6.1
+##
+# WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+                            QMetaObject, QObject, QPoint, QRect,
+                            QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+                           QFont, QFontDatabase, QGradient, QIcon,
+                           QImage, QKeySequence, QLinearGradient, QPainter,
+                           QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QLabel,
+                               QLineEdit, QMainWindow, QMenuBar, QPushButton,
+                               QSizePolicy, QStatusBar, QWidget)
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.horizontalLayout = QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.gridLayout = QGridLayout()
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.labelResult = QLabel(self.centralwidget)
+        self.labelResult.setObjectName(u"labelResult")
+        font = QFont()
+        font.setPointSize(40)
+        self.labelResult.setFont(font)
+        self.labelResult.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout.addWidget(self.labelResult, 0, 0, 1, 1)
+
+        self.gridLayout_2 = QGridLayout()
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.lineName = QLineEdit(self.centralwidget)
+        self.lineName.setObjectName(u"lineName")
+        font1 = QFont()
+        font1.setPointSize(25)
+        self.lineName.setFont(font1)
+
+        self.gridLayout_2.addWidget(self.lineName, 0, 1, 1, 1)
+
+        self.labelName = QLabel(self.centralwidget)
+        self.labelName.setObjectName(u"labelName")
+        self.labelName.setFont(font1)
+
+        self.gridLayout_2.addWidget(self.labelName, 0, 0, 1, 1)
+
+        self.buttonSend = QPushButton(self.centralwidget)
+        self.buttonSend.setObjectName(u"buttonSend")
+        font2 = QFont()
+        font2.setPointSize(20)
+        self.buttonSend.setFont(font2)
+
+        self.gridLayout_2.addWidget(self.buttonSend, 0, 2, 1, 1)
+
+        self.gridLayout.addLayout(self.gridLayout_2, 1, 0, 1, 1)
+
+        self.horizontalLayout.addLayout(self.gridLayout)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 22))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate(
+            "MainWindow", u"MainWindow", None))
+        self.labelResult.setText(QCoreApplication.translate(
+            "MainWindow", u"Exemplo QtDesigner", None))
+        self.lineName.setPlaceholderText(QCoreApplication.translate(
+            "MainWindow", u"Digite seu nome", None))
+        self.labelName.setText(QCoreApplication.translate(
+            "MainWindow", u"Seu nome:", None))
+        self.buttonSend.setText(QCoreApplication.translate(
+            "MainWindow", u"Enviar", None))
+    # retranslateUi
