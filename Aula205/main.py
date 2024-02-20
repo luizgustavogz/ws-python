51f9e64059be9ee4974deec21072f044fa6f1137
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2024-02-20 13:28:12 -0300

    Update main.py

--- Aula205/main.py
+++ Aula205/main.py
@@ -4,11 +4,21 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent
 DB_FILE_NAME = 'db.sqlite3'
 DB_FILE = ROOT_DIR / DB_FILE_NAME
+TABLE_NAME = 'customers'
 
 conn = sqlite3.connect(DB_FILE)
 cursor = conn.cursor()
 
-# SQL
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'age INTEGER,'
+    'weight REAL'
+    ')'
+)
+conn.commit()
 
 cursor.close()
 conn.close()
