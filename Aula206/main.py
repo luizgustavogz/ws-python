dc5277a0d26a4affae75aac8ff53955b9e4565fa
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2024-02-23 12:30:26 -0300

    Create main.py

---
+++ Aula206/main.py
@@ -0,0 +1,5 @@
+# PyMySQL - um cliente MySQL feito em Python Puro
+# Doc: https://pymysql.readthedocs.io/en/latest/
+# Pypy: https://pypi.org/project/pymysql/
+# GitHub: https://github.com/PyMySQL/PyMySQL
+# pip install pymysql
