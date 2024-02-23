4729f2d0e26725527cf641d8b8d521190432fc1b
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2024-02-23 12:37:50 -0300

    Update main.py

--- Aula206/main.py
+++ Aula206/main.py
@@ -3,3 +3,16 @@
 # Pypy: https://pypi.org/project/pymysql/
 # GitHub: https://github.com/PyMySQL/PyMySQL
 # pip install pymysql
+import pymysql
+
+conn = pymysql.connect(
+    host='localhost',
+    user='usuario',
+    password='senha',
+    database='db_python',
+)
+
+with conn:
+    with conn.cursor() as cursor:
+        # SQL
+        print(cursor)
