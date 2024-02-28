4acdba95a41f47b57b353cd851c3c165e7e77a77
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2024-02-28 17:49:55 -0300

    Render home HTML

--- Aula207-django/home/views.py
+++ Aula207-django/home/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def homeView(request):
     print('Home')
-    return HttpResponse('Home do app 1')
+    return render(
+        request,
+        'home/index.html'
+    )
